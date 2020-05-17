0a1070eb513ced2b913edb9054c30b08f1bd413e
neal <neal.lion@icloud.com>
2020-05-17 20:52:16 +0800

    add search card ability

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,25 +1,124 @@
+#include <fstream>
 #include <iostream>
 #include <set>
 #include <sstream>
 
 #include <cqcppsdk/cqcppsdk.h>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 using namespace cq;
 using namespace std;
 using Message = cq::message::Message;
 using MessageSegment = cq::message::MessageSegment;
 
+const string CardMapVersion = "1.0.0.9";
+json cardInfo;
+
+string searchCard(const string &msg) {
+    string searchContent = "";
+
+    // remove prefix
+    if (msg.substr(0, 11) == "/searchcard") {
+        searchContent = msg.substr(11);
+    } else if (msg.substr(0, 6) == "/scard") {
+        searchContent = msg.substr(6);
+    } else if (msg.substr(0, 3) == "/sc") {
+        searchContent = msg.substr(3);
+    } else {
+        logging::warning("错误", "非法查询");
+        return "";
+    }
+
+    // earse spaces
+    auto itor = remove_if(searchContent.begin(), searchContent.end(), ::isspace);
+    searchContent.erase(itor, searchContent.end());
+
+    if (cardInfo.contains(searchContent)) {
+        json targetInfo = cardInfo[searchContent];
+
+        // change the list into string
+        vector<string> categoriesArray = targetInfo["Categories"];
+        string categoriesString = "";
+        for (const string &s : categoriesArray) {
+            categoriesString += s;
+        }
+
+        string returnInfo =
+            "CardId: " + targetInfo["CardId"].get<string>() + '\n' + "Name: " + targetInfo["Name"].get<string>() + '\n'
+            + "Strength: " + to_string(targetInfo["Strength"].get<int>()) + '\n'
+            + "Info: " + targetInfo["Info"].get<string>() + '\n' + "Group: " + targetInfo["Group"].get<string>() + '\n'
+            + "Faction: " + targetInfo["Faction"].get<string>() + '\n' + "Categories: " + categoriesString + '\n';
+        return returnInfo;
+    }
+
+    vector<string> possibleAnswer;
+
+    for (auto it = cardInfo.begin(); it != cardInfo.end(); ++it) {
+        json nestedInfo = *it;
+        string name = nestedInfo["Name"].get<string>();
+        string info = nestedInfo["Name"].get<string>();
+
+        std::size_t foundName=name.find(searchContent);
+        std::size_t foundInfo=info.find(searchContent);
+        if (foundName!=string::npos || foundInfo!=string::npos){
+            possibleAnswer.push_back(nestedInfo["CardId"]);
+        }
+    }
+
+    if (possibleAnswer.size()==0){
+        return "[WARN] 没有找到符合条件的个体。";
+    } else if (possibleAnswer.size()==1){
+        json targetInfo = cardInfo[possibleAnswer[0]];
+
+        // change the list into string
+        vector<string> categoriesArray = targetInfo["Categories"];
+        string categoriesString = "";
+        for (const string &s : categoriesArray) {
+            categoriesString += s;
+        }
+
+        string returnInfo =
+            "CardId: " + targetInfo["CardId"].get<string>() + '\n' + "Name: " + targetInfo["Name"].get<string>() + '\n'
+            + "Strength: " + to_string(targetInfo["Strength"].get<int>()) + '\n'
+            + "Info: " + targetInfo["Info"].get<string>() + '\n' + "Group: " + targetInfo["Group"].get<string>() + '\n'
+            + "Faction: " + targetInfo["Faction"].get<string>() + '\n' + "Categories: " + categoriesString + '\n';
+        return returnInfo;
+    } else {
+        string returnInfo = "符合条件的卡牌有：";
+        for (const string &s : possibleAnswer){
+            returnInfo += ' ' + cardInfo[s]["Name"].get<string>() + '(' + cardInfo[s]["CardId"].get<string>() + ')' + ",";
+        }
+        returnInfo.pop_back();
+        return returnInfo;
+    }
+
+}
+
+bool checkIfSearchCard(const string &msg) {
+    if (msg.substr(0, 11) == "/searchcard" || msg.substr(0, 6) == "/scard" || msg.substr(0, 3) == "/sc") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 CQ_INIT {
-    on_enable([] { logging::info("启用", "插件已启用"); });
+    on_enable([] {
+        ifstream f("/mnt/c/Users/neal/Projects/coolqBot/src/cardInfo.json");
+        cardInfo = json::parse(f);
+        // std::cout << cardInfo;
+        logging::info("启用", "插件已启用");
+    });
 
     on_private_message([](const PrivateMessageEvent &event) {
-        try {
-            auto msgid = send_private_message(event.user_id, event.message); // 直接复读消息
-            logging::info_success("私聊", "私聊消息复读完成, 消息 Id: " + to_string(msgid));
-            send_message(event.target,
-                         MessageSegment::face(111) + "这是通过 message 模块构造的消息~"); // 使用 message 模块构造消息
-        } catch (ApiError &err) {
-            logging::warning("私聊", "私聊消息复读失败, 错误码: " + to_string(err.code));
+        if (checkIfSearchCard(event.message)) {
+            try {
+                const string msg = searchCard(event.message);
+                send_message(event.target, msg); // 使用 message 模块构造消息
+            } catch (ApiError &err) {
+                logging::warning("私聊", "私聊消息复读失败, 错误码: " + to_string(err.code));
+            }
         }
     });
 
@@ -32,12 +131,7 @@ CQ_INIT {
         if (ENABLED_GROUPS.count(event.group_id) == 0) return; // 不在启用的群中, 忽略
 
         try {
-            send_message(event.target, event.message); // 复读
-            auto mem_list = get_group_member_list(event.group_id); // 获取群成员列表
-            string msg;
-            for (auto i = 0; i < min(10, static_cast<int>(mem_list.size())); i++) {
-                msg += "昵称: " + mem_list[i].nickname + "\n"; // 拼接前十个成员的昵称
-            }
+            const string msg = searchCard(event.message);
             send_group_message(event.group_id, msg); // 发送群消息
         } catch (ApiError &) { // 忽略发送失败
         }
@@ -46,21 +140,4 @@ CQ_INIT {
         }
         event.block(); // 阻止当前事件传递到下一个插件
     });
-
-    on_group_upload([](const auto &event) { // 可以使用 auto 自动推断类型
-        stringstream ss;
-        ss << "您上传了一个文件, 文件名: " << event.file.name << ", 大小(字节): " << event.file.size;
-        try {
-            send_message(event.target, ss.str());
-        } catch (ApiError &) {
-        }
-    });
-}
-
-CQ_MENU(menu_demo_1) {
-    logging::info("菜单", "点击菜单1");
-}
-
-CQ_MENU(menu_demo_2) {
-    send_private_message(10000, "测试");
 }
